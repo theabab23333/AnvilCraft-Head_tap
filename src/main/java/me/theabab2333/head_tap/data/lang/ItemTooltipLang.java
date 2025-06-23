d7e9ffc7cbccfbcc1a930364d840cb724ce130ca
theabab23333 <2047477146@qq.com>
2025-06-24 06:58:49 +0800

    同步主分支

--- src/main/java/me/theabab2333/head_tap/data/lang/ItemTooltipLang.java
+++
@@ -1,9 +0,0 @@
-package me.theabab2333.head_tap.data.lang;
-
-import com.tterrag.registrate.providers.RegistrateLangProvider;
-
-public class ItemTooltipLang {
-    public static void init(RegistrateLangProvider provider) {
-        provider.add("item.head_tap.amethyst_hammer.tooltip", "Fragile crystal, deals more damage.");
-    }
-}
